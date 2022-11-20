624a3e993ab3fb7cfe9649eb4060a040b694e136
alvinxv5 <alvinrg051@hotmail.com>
2022-11-19 22:25:56 -0500

    fix structure

--- src/main/java/UI/RulePage.java
+++ src/main/java/UI/RulePage.java
@@ -9,24 +9,24 @@ public class RulePage {
     public void init(){
         ImageIcon bg = new ImageIcon(this.getClass().getResource("/rulesbg.jpg"));
         JLabel myLabel = new JLabel(bg);
-        myLabel.setSize(1280,720);
+        myLabel.setSize(1920,1080);
 
         ImageIcon bg1 = new ImageIcon(this.getClass().getResource("/2.jpg"));
         JLabel myLabel1 = new JLabel(bg1);
-        myLabel1.setBounds(300,150,616,353);
+        myLabel1.setBounds(450,200,616,353);
 
         JButton cont = new JButton("continue");
-        cont.setBounds(600,600,100,50);
+        cont.setBounds(700,700,100,50);
         myLabel.add(myLabel1);
         myLabel.add(cont);
 
         JFrame frame = new JFrame("Rule");
-        frame.add(myLabel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1280, 720);
+        frame.setSize(1920, 1080);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+        frame.add(myLabel);
     }
     public static void main(String[] args) {
         new RulePage().init();
