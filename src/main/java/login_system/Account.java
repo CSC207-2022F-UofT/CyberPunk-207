22da64334c0b941ef5e26dbf61f52e19ed6a0aba
alvinxv5 <alvinrg051@hotmail.com>
2022-11-20 23:50:02 -0500

    initiate login branch

--- src/main/java/login_system/Account.java
+++ src/main/java/login_system/Account.java
@@ -2,17 +2,16 @@ package login_system;
 public class Account {
     private String username;
     private String password;
-    private int wins;
-    private int loses;
-    private int win_rate;
+
 
     public Account(String username, String password){
         this.username = username;
         this.password = password;
-        this.wins = 0;
     }
 
-    public String getPassword() {
-        return this.password;
-    }
+    public String getPassword() {return this.password;}
+
+    public String getUsername() {return username;}
+
+
 }
