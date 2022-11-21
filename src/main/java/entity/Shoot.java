0570e1d4038aa30e09ad874917b884b013dd0873
kurtxue <90428565+kurtxue@users.noreply.github.com>
2022-11-20 21:39:54 -0500

    Update Shoot.java

--- src/main/java/entity/Shoot.java
+++ src/main/java/entity/Shoot.java
@@ -1,18 +1,11 @@
-package entity;
 
 public class Shoot extends basic_card{
 
-    public Shoot(){
-        super();
-    }
-
-    @Override
     public void use(){
-        if (!getTarget().check_dodge()){
-            getTarget().hurted(1);
-        }
+        if (!getTarget().whether_has_dodge()){
+                getTarget().hurted(1);
+            }
     }
-
     @Override
     public String toString(){
         return "Shoot";
