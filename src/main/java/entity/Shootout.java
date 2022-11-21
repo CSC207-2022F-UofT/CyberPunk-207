1ecfab80c9f12c0099b8023674e5aa835bada00b
kurtxue <90428565+kurtxue@users.noreply.github.com>
2022-11-20 21:41:49 -0500

    Update Shootout.java

--- src/main/java/entity/Shootout.java
+++ src/main/java/entity/Shootout.java
@@ -1,13 +1,18 @@
 package entity;
 
-public class Shootout extends strategy_card {
-    @Override
-    public String toString() {
-        return null;
+import manager.Gameboard;
+
+public class Shootout extends strategy_card{
+    public void use(){
+        for (Player p : Gameboard.get_players()) {
+            if (p != getSource() && !p.whether_has_dodge()){
+                p.hurted(1);
+            }
+        }
     }
 
     @Override
-    public void use(int pick_num) {
-
+    public String toString() {
+        return "Shootout";
     }
 }
