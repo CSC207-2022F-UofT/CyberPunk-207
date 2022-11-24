f0c62a80af8028a575a184c382cdbb17c2df566d
alvinxv5 <alvinrg051@hotmail.com>
2022-11-23 19:10:09 -0500

    Fixed execution error of login and rule page

--- src/main/java/UI/GamePage.java
+++ src/main/java/UI/GamePage.java
@@ -1,4 +1,18 @@
 package UI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GamePage {
+    public void init(){
+        JFrame frame = new JFrame("Cyberpunk 207");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(Color.cyan);
+    }
+
+    public static void main(String[] args) {new GamePage().init();}
 }
